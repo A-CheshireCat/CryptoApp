9c87eb387d050bc2475f4a225d9fba81b5ce8f05
alexandra <123159015+A-CheshireCat@users.noreply.github.com>
2024-09-04 16:37:52 +0300

    initial commit

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Alexandra on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
